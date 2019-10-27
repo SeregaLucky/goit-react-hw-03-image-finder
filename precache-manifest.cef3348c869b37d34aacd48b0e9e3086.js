40bcd9863d22782e8d1b2d074888a4279e6c8d88
Sergii Stryzg <strizgsergey@gmail.com>
2019-10-27 13:45:10 +0200

    Updates

---
+++ precache-manifest.cef3348c869b37d34aacd48b0e9e3086.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebcc65dc2f1ea9c5c52ae8d804826b19",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "a6ffbbaa24def22f9296",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.a5851894.chunk.css"
+  },
+  {
+    "revision": "3e354bc17f2191da12f1",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.19352e64.chunk.js"
+  },
+  {
+    "revision": "a6ffbbaa24def22f9296",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.4c0b81bf.chunk.js"
+  },
+  {
+    "revision": "26f980cacd962de52565",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.9d9945f4.js"
+  }
+]);
